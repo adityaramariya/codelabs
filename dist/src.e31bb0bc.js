3d4f32648c6ef508c732f8b431b200d9d9883b20
aditya <aditya.ramariya@gmail.com>
2020-10-05 00:29:32 +0530

    changes as discussed

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -30507,7 +30507,37 @@ var myFullpage = new _fullpage.default('#fullpage', {
   menu: '#menu',
   scrollHorizontally: true,
   responsiveWidth: 991,
-  css3: true
+  css3: true,
+  scrollingSpeed: 500,
+  onLeave: function onLeave(origin) {
+    var body = document.body; //after leaving section 2
+
+    console.log(origin.index);
+
+    if (origin.index > 1) {
+      body.classList.add("fixed__header");
+    } else if (origin.index === 1) {
+      body.classList.remove("fixed__header");
+    }
+  }
+});
+$(document).on('click', '#scrollToTop', function () {
+  fullpage_api.moveTo('home', 1);
+});
+$(document).on('click', '.scrollToContact', function () {
+  fullpage_api.moveTo('contact', 1);
+}); // function to close navbar on the click of outside
+
+$(document).ready(function () {
+  $(document).click(function (event) {
+    var clickover = $(event.target);
+
+    var _opened = $(".navbar-collapse").hasClass("show");
+
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+      $("button.navbar-toggler").click();
+    }
+  });
 });
 
 function isMobile() {
@@ -30548,7 +30578,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52071" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54617" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import 'bootstrap/dist/js/bootstrap';
-import 'babel-polyfill'
-import fullpage from './vendor/fullpage'
+import 'babel-polyfill';
+import fullpage from './vendor/fullpage';
 
 var myFullpage = new fullpage('#fullpage', {
     anchors: ['home', 'about', 'about', 'work', 'services', 'services', 'contact'],
     menu: '#menu',
     scrollHorizontally: true,
     responsiveWidth: 991,
-    css3: true
+    css3: true,
+     scrollingSpeed: 500,
+
+    onLeave: function(origin){
+        let body = document.body;
+        //after leaving section 2
+        console.log(origin.index)
+        if(origin.index > 1)
+        {
+            body.classList.add("fixed__header");
+        }
+        else if(origin.index === 1)
+        {
+            body.classList.remove("fixed__header");
+        }
+    }
+});
+
+$(document).on('click', '#scrollToTop', function(){
+    fullpage_api.moveTo('home', 1);
+});
+
+$(document).on('click', '.scrollToContact', function(){
+    fullpage_api.moveTo('contact', 1);
+});
+
+
+// function to close navbar on the click of outside
+
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $("button.navbar-toggler").click();
+        }
+    });
 });
 
 
@@ -22,4 +58,5 @@ if (isMobile()) {
         $('.navbar-collapse').removeClass('show');
     });
 }
+
 
